e40f94cd3335104274a9e020649597c1bba7e15e
Stefan Müller zzeroo <co@zzeroo.com>
2025-06-20 18:15:04 +0200

    allow clippy::result_large_err

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::result_large_err)]
+
 use loco_meter::app::App;
 use loco_rs::cli;
 use migration::Migrator;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::result_large_err)]
+
 pub mod app;
 pub mod controllers;
 pub mod data;
